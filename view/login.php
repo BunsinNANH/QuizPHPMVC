806b96bc14dc2576e65bb360c8930e8992ad21c2
Bunsin.Nanh <Bunsin.Nanh@pnc.lan>
2019-03-25 10:57:26 +0700

    first commit

---
+++ view/login.php
@@ -0,0 +1,18 @@
+<div class="container">
+    <form method="post" action="index.php?action=loginVaildate">
+        <div id="div_login">
+            <h1>Login</h1>
+            <div>
+                <input type="text" class="textbox" id="txt_uname" name="txt_uname" placeholder="Username" />
+            </div>
+            <div>
+                <input type="password" class="textbox" id="txt_uname" name="txt_pwd" placeholder="Password"/>
+            </div>
+            <div class="form-group">
+                <input type="submit" value="Login Now" name="but_submit" id="but_submit" />
+            </div>
+        </div>
+    </form>
+    <br>
+    <h4 class="text-center">You Don't Have Account? <a href="index.php?action=register">Register Now</a></h4>
+</div>
